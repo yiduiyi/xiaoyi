e439cc53f82d8937fda36ca146c58f55d4ed8400
yiduiyi <33659384+yiduiyi@users.noreply.github.com>
2017-12-19 22:31:00 +0800

    修改部分配置，增加前端调试接口（测试）
    修改部分配置，增加前端调试接口（测试）

---
+++ workspace/manager/src/main/java/com/xiaoyi/ma/action/AccountAction.java
@@ -0,0 +1,46 @@
+package com.xiaoyi.ma.action;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.xiaoyi.ma.domain.Account;
+import com.xiaoyi.ma.service.IAccountService;
+
+@Controller
+@RequestMapping("/account")
+public class AccountAction {
+	@Resource
+	private IAccountService accountService;
+	
+	@RequestMapping(value="/showAccountName",method=RequestMethod.POST)
+	@ResponseBody
+	public  JSONObject getAccountById(HttpServletRequest request
+			,HttpServletResponse response,
+			@RequestBody JSONObject reqData) {
+		JSONObject result = new JSONObject();
+		Account account =
+				accountService.getAccountById(reqData.getString("accountId"));
+		result.put("account", account);
+		
+		return result;
+	}
+	
+	@RequestMapping("/showUser")  
+    public String toIndex(HttpServletRequest request,Model model){  
+        String userId = request.getParameter("id");  
+        Account account = this.accountService.getAccountById(userId);  
+        model.addAttribute("user", account);  
+        return "index";  
+	}
+
+    
+}
