d625016584c81a72739e321b31579219d116908b
dengzhihua <dengzhihua@DESKTOP-95V0G1B>
2018-01-16 15:48:48 +0800

    重命名订单管理类

--- workspace/manager/src/main/java/com/xiaoyi/manager/dao/order/IOrderManageDao.java
+++ workspace/manager/src/main/java/com/xiaoyi/manager/dao/order/IOrderManageDao.java
@@ -5,7 +5,12 @@ import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
 
-public interface ITeachingDao {
+public interface IOrderManageDao {
 	//查找任教老师
     List<JSONObject> selectTeachersByConditions(Map<String,Object> params);
+    
+    List<JSONObject> selectCoursesByGradeId(String gradeId);
+    
+    //根据教学任务查找老师
+    List<JSONObject> selectTeachersByTeachingIds(List<String> teachingIds);
 }
