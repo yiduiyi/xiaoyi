4356f10402bae6f5a1425ccada748169cb99b8c4
yiduiyi <33659384+yiduiyi@users.noreply.github.com>
2018-01-18 22:16:19 +0800

    完善查询老师端订单关系接口

--- workspace/manager/src/main/java/com/xiaoyi/teacher/dao/ITeachingRecordDao.java
+++ workspace/manager/src/main/java/com/xiaoyi/teacher/dao/ITeachingRecordDao.java
@@ -3,7 +3,12 @@ package com.xiaoyi.teacher.dao;
 import java.util.List;
 
 import com.alibaba.fastjson.JSONObject;
+import com.xiaoyi.manager.domain.TeachingRecord;
 
 public interface ITeachingRecordDao {
+	//获取老师关联的订单
 	List<JSONObject> selectRecordsByTid(String teacherId);
+
+	//录入老师带课记录
+	int insertTeachingRecords(List<TeachingRecord> tRecords);
 }
