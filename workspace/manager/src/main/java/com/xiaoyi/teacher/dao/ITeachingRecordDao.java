c9cf534909daedf33618ca33d320ac57e76d3db7
yiduiyi <33659384+yiduiyi@users.noreply.github.com>
2018-03-12 22:33:59 +0800

    实现获取老师提现记录列表

--- workspace/manager/src/main/java/com/xiaoyi/teacher/dao/ITeachingRecordDao.java
+++ workspace/manager/src/main/java/com/xiaoyi/teacher/dao/ITeachingRecordDao.java
@@ -1,14 +1,19 @@
 package com.xiaoyi.teacher.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
+import com.xiaoyi.teacher.domain.LessonTrade;
 import com.xiaoyi.teacher.domain.TeachingRecord;
 
 public interface ITeachingRecordDao {
 	//获取老师关联的订单
 	List<JSONObject> selectRecordsByTid(String teacherId);
-
+	
+	//查询老师提现记录
+	List<JSONObject> selectLSRecordByParams(Map<String,Object> params);
+	
 	//录入老师带课记录
 	int insertTeachingRecords(List<TeachingRecord> tRecords);
 	
