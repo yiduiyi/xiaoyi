de5bed1d19c6ac6976f9542d0d6eddaf701fa02a
1376005213 <1376005213@qq.com>
2018-08-29 08:58:34 +0800

    提交接口修改与优化

--- workspace/manager/src/main/java/com/xiaoyi/common/utils/MathUtils.java
+++ workspace/manager/src/main/java/com/xiaoyi/common/utils/MathUtils.java
@@ -13,11 +13,13 @@ public class MathUtils {
 	 * 求百分比
 	 */
 	public static String percentage(int num1, int num2) {
+		StringBuilder result = new StringBuilder();
 		// 创建一个数值格式化对象
 		NumberFormat numberFormat = NumberFormat.getInstance();
 		// 设置精确到小数点后2位
 		numberFormat.setMaximumFractionDigits(2);
-		String result = numberFormat.format((float) num1 / (float) num2 * 100);
-		return result;
+		String percent = numberFormat.format((float) num1 / (float) num2 * 100);
+		result = result.append(percent).append("%");
+		return result.toString();
 	}
 }
--- workspace/manager/src/test/java/TestMonitor.java
+++ workspace/manager/src/test/java/TestMonitor.java
@@ -8,19 +8,24 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.alibaba.fastjson.JSONObject;
-import com.xiaoyi.manager.service.MonitorService;
+import com.xiaoyi.manager.service.IOrderService;
+import com.xiaoyi.manager.service.IMonitorService;
+import com.xiaoyi.manager.service.impl.OrderServiceImpl;
 
 @RunWith(JUnit4ClassRunner.class)     //琛ㄧず缁ф壙浜哠pringJUnit4ClassRunner绫�  
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 public class TestMonitor {
 	
 	@Resource
-	private MonitorService monitorService;
+	private IMonitorService monitorService;
+	
+	@Resource
+	private IOrderService orderService;
 	
 	@Test
 	public void testGetTeachingProcess() {
 		JSONObject reqData = new JSONObject();
-		reqData.put("teacherName", "颜");
+		/*reqData.put("teacherName", "吕");*/
 		List<JSONObject> data = monitorService.getTeachingProcess(reqData);
 		if(CollectionUtils.isNotEmpty(data)) {
 			for (JSONObject jsonObject : data) {
@@ -31,9 +36,20 @@ public class TestMonitor {
 	@Test
 	public void testGetCustomFeedback() {
 		JSONObject reqData = new JSONObject();
-		reqData.put("teachingId", "5a657d14-c207-411d-8bdf-cf6cb8e0db70");
-		reqData.put("queryMonth", "2018-07");
+		reqData.put("teachingId", "c49a1c82-ab52-493f-a1d4-abd1780d9281");
+		reqData.put("queryMonth", "2018-9-28");
 		JSONObject data = monitorService.getCustomFeedback(reqData);
 		System.out.println(data);
+	}
+	/*"2018-8-28"
+	teachingId
+	:
+	"a9569bc1-152c-4499-809b-789ba9c383f7"*/
+
+	
+	@Test
+	public void testGetOrderList() {
+		JSONObject reqData = new JSONObject();
+		List<JSONObject> data  = orderService.getOrderList(reqData);
 	}
 }
